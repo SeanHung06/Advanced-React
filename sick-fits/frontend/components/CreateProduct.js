71ae8d819064aeb02c55f48b9f7bea329591d9f2
洪立軒 <u8351574@gmail.com>
2021-10-07 15:05:29 +0800

    Lesson 25 Refetch Queries after Successful Mutation

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
@@ -41,8 +42,10 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
+  console.log('s');
   return (
     <Form
       onSubmit={async (e) => {
@@ -50,8 +53,8 @@ export default function CreateProduct() {
         console.log(inputs);
 
         // Submit the inputfields to the backend
-        const res = await createProduct();
-        console.log(res);
+        await createProduct();
+        clearForm();
       }}
     >
       <DisplayError error={error} />
@@ -98,9 +101,7 @@ export default function CreateProduct() {
             onChange={handleChange}
           />
         </label>
-        <button type="submit" onClick={clearForm}>
-          +Add Product
-        </button>
+        <button type="submit">+Add Product</button>
       </fieldset>
     </Form>
   );
