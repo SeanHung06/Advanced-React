b984317f5eda5008cab7d24c805ca3e0c2a6f7f1
洪立軒 <u8351574@gmail.com>
2021-10-12 13:25:00 +0800

    Lesson 33 Pagination Dynamic Routing

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,4 +1,5 @@
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
@@ -16,26 +17,27 @@ const PAGINATION_QUERY = gql`
 
 export default function Pagination({ page }) {
   const { error, loading, data } = useQuery(PAGINATION_QUERY);
-  const { count } = data._allProductsMeta;
-
-  const pageCount = Math.ceil(count / perPage);
-  console.log(count, pageCount);
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
+
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  console.log(count, pageCount);
+
   return (
     <PaginationStyles>
       <Head>
         <title>Sick Fits - Page {page} of __ </title>
       </Head>
 
-      <Link href={`/product/${page - 1}`}>
+      <Link href={`/products/${page - 1}`}>
         <a aria-disabled={page <= 1}> Prev</a>
       </Link>
       <p>
         Page {page} of {pageCount}
       </p>
       <p> {count} Items Total</p>
-      <Link href={`/product/${page + 1}`}>
+      <Link href={`/products/${page + 1}`}>
         <a aria-disabled={page >= pageCount}>Next</a>
       </Link>
     </PaginationStyles>
