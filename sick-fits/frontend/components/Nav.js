fee459eea528a0f3a08a279a8ff4f1625e6af62a
洪立軒 <u8351574@gmail.com>
2021-10-14 17:59:48 +0800

    Lesson 36 Querying the current Use

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,13 +1,26 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
 export default function Nav() {
+  const user = useUser();
+  console.log(user);
   return (
     <NavStyles>
       <Link href="/products">products</Link>
-      <Link href="/sell">sell</Link>
-      <Link href="/order">order</Link>
-      <Link href="/account">account</Link>
+      {user && (
+        <>
+          <Link href="/sell">sell</Link>
+          <Link href="/order">order</Link>
+          <Link href="/account">account</Link>
+        </>
+      )}
+
+      {!user && (
+        <>
+          <Link href="/SignIn">SignIn</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
