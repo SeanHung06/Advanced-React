63bc8cf1fa2f642a1e6961b8e69f79ea1a06d3bf
洪立軒 <u8351574@gmail.com>
2022-01-19 00:19:17 +0800

    Permissions Access Functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,3 +1,4 @@
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 
@@ -5,4 +6,19 @@ export default function isSignedIn({ session}: ListAccessArgs){
     return !!session;// If there is no session then it will return false with double !
 
 
+}
+// To avoid Typing every permission check 
+const generatedPermissions = Object.fromEntries(permissionsList.map(permissions =>[
+    permission,
+    function({session}:ListAccessArgs){
+        return session?.data.role.[permission];
+    }
+]))
+// Permissions check if someone meets a criteria - yes or no 
+export const permissions = {
+    ...generatedPermissions,
+    // Shown of adding additional permission 
+    isAwesome({session}: ListAccessArgs): boolean{
+        return session?.data.name.includes('wes');
+    },
 }
