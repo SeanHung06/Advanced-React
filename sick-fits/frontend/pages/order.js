15a37c795ef51c0b061d4bb3655b241cd79d0141
洪立軒 <u8351574@gmail.com>
2022-01-18 15:01:23 +0800

    Lesson 60 Displaying All Orders

--- sick-fits/frontend/pages/order.js
+++ sick-fits/frontend/pages/order.js
@@ -1,7 +1,87 @@
-export default function OrderPage() {
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import Head from 'next/head';
+import Link from 'next/link';
+import styled from 'styled-components';
+import OrderItemStyles from '../components/styles/OrderItemStyles';
+import ErrorMessage from '../components/ErrorMessage';
+import OrderStyles from '../components/styles/OrderStyles';
+import formatMoney from '../lib/formatMoney';
+// Give me the specific Order where the ID is equal to the ID that we passed in the Query
+
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    allOrders {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+const OrderUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 4rem;
+`;
+
+function countItemsInAnOrder(order) {
+  return order.items.reduce((tally, item) => tally + item.quantity, 0);
+}
+
+export default function OrdersPage() {
+  const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+  const { allOrders } = data;
+  // Props to query the order ID and destructure to {query }
   return (
-    <>
-      <p>Hey!</p>
-    </>
+    <div>
+      <Head>
+        <title>Your Orders ({allOrders.length})</title>
+      </Head>
+      <h2> You have {allOrders.length}</h2>
+      <OrderUl>
+        {allOrders.map((order) => (
+          <OrderItemStyles>
+            <Link href={`/order/${order.id}`}>
+              <a>
+                <div className="order-meta">
+                  <p>{countItemsInAnOrder(order)} Items</p>
+                  <p>
+                    {order.items.length} Product
+                    {order.items.length === 1 ? '' : 's'}
+                  </p>
+                  <p> {formatMoney(order.total)}</p>
+                </div>
+                <div className="images">
+                  {order.items.map((item) => (
+                    <img
+                      key={`image-${item.id}`}
+                      src={item.photo?.image?.publicUrlTransformed} // Question mark just in case there is no image associated with that product and it's not going to break it
+                      alt={item.name}
+                    />
+                  ))}
+                </div>
+              </a>
+            </Link>
+          </OrderItemStyles>
+        ))}
+      </OrderUl>
+    </div>
   );
 }
