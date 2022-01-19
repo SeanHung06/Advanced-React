54c30e58dd0d485bb2172d7ba0b3251276eb3fbb
洪立軒 <u8351574@gmail.com>
2022-01-19 16:28:15 +0800

    Lesson 66 Getting Meta - Roles based Roles and Hiding the UI of Roles

--- sick-fits/backend/schemas/Role.ts
+++ sick-fits/backend/schemas/Role.ts
@@ -1,9 +1,23 @@
 import {list} from '@keystone-next/keystone/schema';
 import {relationship, text} from '@keystone-next/fields';
 import { permissionFields } from './fields';
+import { permissions} from '../access';
 
 
 export const Role = list({
+    // To limit the Role Access 
+    access: {
+        create: permissions.canManageRoles,
+        read: permissions.canManageRoles,
+        update: permissions.canManageRoles,
+        delete: permissions.canManageRoles,
+    },
+    // To hide the UI of Roles column to those with permissions
+    ui: {
+      hideCreate: (args) => !permissions.canManageRoles(args),
+      hideDelete: (args) => !permissions.canManageRoles(args),
+      isHidden: (args) => !permissions.canManageRoles(args),
+    },
     fields: {
         name: text({ isRequired: true }),
         ...permissionFields,// Import Permission Fields File in here
